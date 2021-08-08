dc09a6830aba746f8fac65e526465c394687bb8d
Binita <binitaparikh3282@gmail.com>
2021-08-08 13:35:13 +0530

    first

---
+++ application/logs/log-2021-08-07.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-08-07 16:49:43 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'shop' C:\xampp\htdocs\shop\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2021-08-07 16:49:43 --> Unable to connect to the database
